23350c49cfba0d77fc83371c3fcfe0f47c6a5213
Leslie Rados <leslie.rados@gmail.com>
2024-01-24 13:24:59 +0100

    installation cloudinary

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,7 +1,7 @@
 require "active_support/core_ext/integer/time"
 
 Rails.application.configure do
-  config.action_mailer.default_url_options = { host: "https://git.heroku.com/monsite-leslierds.git" }
+  config.action_mailer.default_url_options = { host: "https://monsite-leslierds-e2435330d20b.herokuapp.com/" }
   # Settings specified here will take precedence over those in config/application.rb.
 
   # Code is not reloaded between requests.
