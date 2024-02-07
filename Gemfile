4b1b99b4565dc7d60f3fc91fad78f4a5d05f8882
Leslie Rados <leslie.rados@gmail.com>
2024-02-07 16:15:57 +0100

    sendgrid initialisation

--- Gemfile
+++ Gemfile
@@ -72,7 +72,6 @@ end
 group :development do
   # Use console on exceptions pages [https://github.com/rails/web-console]
   gem "web-console"
-
   # Add speed badges [https://github.com/MiniProfiler/rack-mini-profiler]
   # gem "rack-mini-profiler"
 
@@ -84,5 +83,4 @@ group :test do
   # Use system testing [https://guides.rubyonrails.org/testing.html#system-testing]
   gem "capybara"
   gem "selenium-webdriver"
-
 end
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -8,10 +8,10 @@ Rails.application.configure do
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
     address:              'smtp.sendgrid.net',
-    port:                 587,
+    port:                 465,
     authentication:       :plain,
-    user_name:            ENV['SENDGRID_USERNAME'],
-    password:             ENV['SENDGRID_PASSWORD'],
+    user_name:            'apikey', # C'est littéralement le mot 'apikey', pas une placeholder
+    password:             ENV['SENDGRID_API_KEY'],
     domain:               'myikigai.fr',
     enable_starttls_auto: true
   }
