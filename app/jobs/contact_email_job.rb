fb0f2f1946875cdddb5dfa2337b3a8fd5745d4fd
Leslie Rados <leslie.rados@gmail.com>
2024-02-08 11:35:16 +0100

    branche master cleaner

--- app/jobs/contact_email_job.rb
+++
@@ -1,7 +0,0 @@
-class ContactEmailJob < ApplicationJob
-  queue_as :default
-
-  def perform(contact)
-    ContactMailer.with(contact: contact).new_contact_email.deliver_later
-  end
-end
--- app/mailers/contact_mailer.rb
+++
@@ -1,8 +0,0 @@
-class ContactMailer < ApplicationMailer
-  default from: 'notifications@example.com'
-
-  def contact_email(contact)
-    @contact = contact
-    mail(to: 'leslie.rados@myikigai.fr', subject: 'Nouveau message de contact')
-  end
-end
