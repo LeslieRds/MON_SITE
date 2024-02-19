9aa9da18cee041db19af8254d98ab2c9e0da3fed
Leslie Rados <leslie.rados@gmail.com>
2024-02-19 09:41:44 +0100

    trad en cours

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,12 +1,17 @@
 class ApplicationController < ActionController::Base
-    before_action :set_locale
+  before_action :set_locale
+  helper_method :new_locale # Ceci rend la méthode accessible dans les vues comme un helper
 
-    def set_locale
-      I18n.locale = session[:locale] || I18n.default_locale
-    end
+  def set_locale
+    I18n.locale = session[:locale] || I18n.default_locale
+  end
 
-    def switch_language
-      session[:locale] = session[:locale] == 'fr' ? 'en' : 'fr'
-      redirect_to request.referer || root_url
-    end
+  def switch_language
+    session[:locale] = session[:locale] == 'fr' ? 'en' : 'fr'
+    redirect_to request.referer || root_url
+  end
+
+  def new_locale
+    I18n.locale == :fr ? 'en' : 'fr'
+  end
 end
